766ace99d7e9c85c2339265faee18d1662f3428c
YounmiSon <symym629@gmail.com>
2022-11-14 17:14:52 +0900

    user DB 생성

--- back/public/index.js
+++ back/public/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 const config = require("../config/config");
 const content = require("./content");
+const user = require("./user");
 
 const sequelize = new Sequelize(
   config.dev.database,
@@ -13,7 +14,9 @@ const db = {};
 
 db.sequelize = sequelize;
 db.content = content;
+db.user = user;
 
 content.init(sequelize);
+user.init(sequelize);
 
 module.exports = db;
---
+++ back/public/user.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        userPw: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: "User",
+        tableName: "users",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+}
+module.exports = User;
