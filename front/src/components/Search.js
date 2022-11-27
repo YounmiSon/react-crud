e27ba4d1f35d17cc97279f89bcb7978b68f35a9d
YounmiSon <symym629@gmail.com>
2022-11-27 21:54:35 +0900

    검색 기능 하는 중..

--- front/src/components/Search.js
+++ front/src/components/Search.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const searchHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    if (search === null || search === "") {
+    }
+  };
   return (
     <div className="border-[1px] border-gray w-[400px] flex justify-center items-center p-6 rounded-md mt-4">
       <select className="mr-4">
@@ -8,8 +20,14 @@ const Search = () => {
         <option>작성자</option>
         <option>내용</option>
       </select>
-      <input className="border-[1px] border-black" />
-      <button className="bg-gray-200 w-12 h-8 ml-4 rounded-md">검색</button>
+      <form onSubmit={(e) => onSearch(e)}>
+        <input
+          className="border-[1px] border-black"
+          onChange={searchHandler}
+          value={search}
+        />
+        <button className="bg-gray-200 w-12 h-8 ml-4 rounded-md">검색</button>
+      </form>
     </div>
   );
 };
